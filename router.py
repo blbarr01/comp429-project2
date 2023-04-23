814e5a768cc63786b7ca0be301f36aaafca3afda
Brian <bbarr0101@gmail.com>
2023-04-23 16:07:16 -0700

    writing of main ui in progress

---
+++ router.py
@@ -0,0 +1,28 @@
+
+
+
+
+def showPrompt():
+    print("server")
+    print()
+
+
+
+if __name__ == '__main__':
+    print("hello")
+#read Init_files  
+
+#create topology 
+    def createTopology():
+        print("make me a topology")
+#broadcast update 
+
+#run main loop
+    while True:
+        #display server options
+        showPrompt()
+        user_input = input("make selection:")
+
+
+#broadcast update 
+
