c1fc70a6f7fdb5516317eb1da74050e42f9116c4
Brian <bbarr0101@gmail.com>
2023-04-24 09:34:10 -0700

    implements main program user loop

--- router.py
+++ router.py
@@ -3,26 +3,66 @@
 
 
 def showPrompt():
-    print("server")
-    print()
+    print("server -t <file-name> -i <interval>")     
+    print("update <server-ID1> <server-ID2> <link-cost>")
+    print("step") #send packets to nieghbors 
+    print("packets")
+    print("display")
+    print("disable <server-id>")
+    print("crash")
+    print("help")
+    print("exit")
+
+#read Init file  
+def initServer(filePath, timeInterval):
+    print("initializing router table")
+    print("from init method", filePath)
+    print("from init method", timeInterval)
+
+    #broadcast update 
+def updateEdge():
+    print("updating edge")
 
 
+def step():
+    print("sending packets to the neighbors")
+
+def displayPackets():
+    print("you've received this many packets")
+
+def displayRoutingTable():
+    print("here are all the routes i got")
+
+def disableServer(serverID):
+    print(f"we'll shutdown server {serverID}")
+
+def simulateCrash():
+    print("simulating a crash")
 
 if __name__ == '__main__':
-    print("hello")
-#read Init_files  
-
-#create topology 
-    def createTopology():
-        print("make me a topology")
-#broadcast update 
-
-#run main loop
+    showPrompt()
+    #run main loop
     while True:
         #display server options
-        showPrompt()
         user_input = input("make selection:")
+        input_arr = user_input.split()
+        if input_arr[0] == "server":
+            initServer(input_arr[2], input_arr[4])
+        elif input_arr[0] == "update":
+            updateEdge()
+        elif input_arr[0] == "step":
+            step()
+        elif input_arr[0] == "packets":
+            displayPackets()
+        elif input_arr[0] == "display":
+            displayRoutingTable()
+        elif input_arr[0] == "disable":
+            disableServer(input_arr[1])
+        elif input_arr[0] == "crash":
+            simulateCrash()
+        elif input_arr[0] == "help":
+            showPrompt()
+        elif input_arr[0] == "exit":
+            exit(1)
 
-
-#broadcast update 
 
